b3227f9bbaff9ec8764dd8959910b4f9635f293b
alexey.boyko <alexey.boyko@vniira.spb.su>
2024-05-08 13:34:41 +0300

    Start fuiling create func2

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from typing import Annotated
-from sqlalchemy import Insert
+from sqlalchemy import insert
 from slugify import slugify
 
 from app.backend.db_depends import get_db
@@ -19,7 +19,12 @@ async def get_all_categories():
 
 @router.post('/create')
 async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory):
-    ...
+    db.execute(insert(Category)).values(name=create_category.name, parent_id=create_category.parent_id, slug=slugify(create_category.name))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
 
 
 @router.put('/update_category')
