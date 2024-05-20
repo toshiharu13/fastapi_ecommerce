72fe5bbd0ba2bbbfff8ffc8d7449d8cae8f9754c
alexey.boyko <alexey.boyko@vniira.spb.su>
2024-05-20 15:11:58 +0300

    Add custom auth

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routers import category, products
+from app.routers import category, products, auth
 
 app = FastAPI()
+
 
 @app.get('/')
 async def welcome() -> dict:
@@ -10,3 +11,4 @@ async def welcome() -> dict:
 
 app.include_router(category.router)
 app.include_router(products.router)
+app.include_router(auth.router)
