a5c77ee7ea6118b7f4eefb34ed73a63e64262c02
alexey.boyko <alexey.boyko@vniira.spb.su>
2024-06-10 13:57:21 +0300

    Add container cooperation

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_async_engine('postgresql+asyncpg://ecommerce:ecommerce@localhost:5432/ecommerce', echo=True)
+engine = create_async_engine('postgresql+asyncpg://postgres_user:postgres_password@db:5432/postgres_database', echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
